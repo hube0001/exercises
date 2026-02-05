408d01517eedc5d473df8e9402429331c2ee23c6
hube0001 <hube0001@stud.kea.dk>
2026-02-05 07:08:42 +0100

    filter made

--- filter_oevelse_starter_files_table/script.js
+++ filter_oevelse_starter_files_table/script.js
@@ -10,19 +10,51 @@ const vehicles = [
   { type: "Knallert", fuel: "Benzin", passengers: 1, ownedBy: "Jonas" },
   { type: "Løbehjul", passengers: 1, isElectric: true },
 ];
+
 const tbodyPointer = document.querySelector("tbody");
+const elVehicle = vehicles.filter((vehicle) => vehicle.isElectric);
+const vehicleSeats = vehicles.filter((vehicle) => vehicle.passengers > 2);
+const jonasBiler = vehicles.filter((vehicle) => vehicle.isElectric && vehicle.ownedBy === "Jonas");
+const ryeBread = vehicles.filter((vehicle) => vehicle.fuel === "Rugbrød" && vehicle.passengers > 1);
+
 showTheseVehicles(vehicles);
 
 function showTheseVehicles(arr) {
   arr.forEach((each) => {
     tbodyPointer.innerHTML += `<tr>
   <td>${each.type}</td>
-  <td>${each.fuel}</td>
+  <td>${each.fuel ? each.fuel : "-"}</td>
   <td>${each.passengers}</td> 
-  <td>${each.stops}</td>
-  <td>${each.ownedBy}</td>
-  <td>${each.isElectric}</td>
-  <td>${each.isTandem}</td>
+  <td>${each.stops ? each.stops : "-"}</td>
+  <td>${each.ownedBy ? each.ownedBy : "-"}</td>
+  <td>${each.isElectric ? "Electric" : "unknown"}</td>
+  <td>${each.isTandem ? "Tandem" : "unknown"}</td>
 </tr>`;
   });
 }
+
+// Få fat i alle knapperne
+document.querySelector("#showAll").addEventListener("click", () => {
+  tbodyPointer.innerHTML = ""; // Ryd tabellen først
+  showTheseVehicles(vehicles);
+});
+
+document.querySelector("#showElectric").addEventListener("click", () => {
+  tbodyPointer.innerHTML = "";
+  showTheseVehicles(elVehicle);
+});
+
+document.querySelector("#showSeats").addEventListener("click", () => {
+  tbodyPointer.innerHTML = "";
+  showTheseVehicles(vehicleSeats);
+});
+
+document.querySelector("#showJonas").addEventListener("click", () => {
+  tbodyPointer.innerHTML = "";
+  showTheseVehicles(jonasBiler);
+});
+
+document.querySelector("#showRye").addEventListener("click", () => {
+  tbodyPointer.innerHTML = "";
+  showTheseVehicles(ryeBread);
+});
